b9774ae284deb3616032dc16a007daba9fb8fa57
pyetrosoares150-max <pyetrosoares150@gmail.com>
2026-08-26 01:21:25 -0300

    adicionando o favicon talvez eu ainda mude ele

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ const myObserver = new IntersectionObserver((entries) => {
 
 const elements = document.querySelectorAll('.hidden')
 elements.forEach((element) => myObserver.observe(element))
+
+
+
+
+
+const myObservador = new IntersectionObserver((entrie) => {
+    entrie.forEach((enry) => {
+        if (enry.isIntersecting === true) {
+            enry.target.classList.add('mostr')
+        } else {
+            enry.target.classList.remove('mostr')
+        }
+    })
+})
+
+
+const elementos = document.querySelectorAll('.escond')
+elementos.forEach((elementos) => myObservador.observe(elementos))
